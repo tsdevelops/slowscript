2fcb5180a3b5e1fe5e00222b6b7448649fdb4409
tsdevelops <tsdevelops+github@gmail.com>
2016-04-01 14:54:52 -0400

    Add basic Context() constructor with errors

--- run.js
+++ run.js
@@ -5,5 +5,36 @@ Copyright © 2016 TSDevelops.
 
 // Run everything in an anonymous function to prevent inadvertent window modification
 (function() {
+  var undef;
+  var exit = function(isError, data) {
+    if (isError) {
+      return {
+        type: 'error',
+        msg:  '' + data
+      };
+    }
+    else {
+      return {
+        type: 'return',
+        val:  data
+      }
+    }
+  };
   
+  var Context = function() {
+    var keys = [];
+    var vals = [];
+    
+    this.add = function(key, val) {
+      if (keys.length !== vals.length) {
+        return exit('InternalError: context keys and vals lengths are not identical.');
+      }
+      if (keys.indexOf(val) === -1) {
+        keys.push(key);
+        vals.push(val);
+        return exit();
+      }
+      return exit('DeclarationError: context already contains "' + key + '" as a key.');
+    };
+  };
 })();
