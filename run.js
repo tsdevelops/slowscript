0f491947c2637379c13308ad7cbc13d184bc07a7
tsdevelops <tsdevelops+github@gmail.com>
2016-04-01 14:39:24 -0400

    Add basic anonymous function to run.js

--- run.js
+++ run.js
@@ -2,3 +2,8 @@
 This file was created on Friday, April 1 by TSDevelops.
 Copyright © 2016 TSDevelops.
 */
+
+// Run everything in an anonymous function to prevent inadvertent window modification
+(function() {
+  
+})();
